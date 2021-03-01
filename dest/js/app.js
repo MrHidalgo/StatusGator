58b8552a8a0801b3af0356256b1dfe6c718c26ac
Andrii <Luckyhidalgo@gmail.com>
2021-03-01 13:49:47 +0200

    Solution -> Education page ::  - created "empty" section - w\o adaptive.

--- dest/js/app.js
+++ dest/js/app.js
@@ -20,44 +20,65 @@
  */
 var initPreventBehavior = function initPreventBehavior() {
 
-	var link = document.querySelectorAll("a");
+  var link = document.querySelectorAll("a");
 
-	link.forEach(function (val, idx) {
+  link.forEach(function (val, idx) {
 
-		val.addEventListener("click", function (e) {
-			if (val.getAttribute("href") === "#") {
-				e.preventDefault();
-			}
-		});
-	});
+    val.addEventListener("click", function (e) {
+      if (val.getAttribute("href") === "#") {
+        e.preventDefault();
+      }
+    });
+  });
+};
+
+/**
+ * @name initSwiper
+ *
+ * @description initialize Swiper
+ */
+var initSwiper = function initSwiper() {
+
+  new Swiper('.reviewsSlider', {
+    loop: false,
+    effect: 'slide',
+    speed: 800,
+    slidesPerView: 1,
+    spaceBetween: 30,
+    navigation: {
+      nextEl: '.swiper-button-next',
+      prevEl: '.swiper-button-prev'
+    }
+  });
 };
 
 /**
  * @description Document DOM ready.
  */
 (function () {
-	/*
- * CALLBACK :: start
- * ============================================= */
+  /*
+  * CALLBACK :: start
+  * ============================================= */
 
-	/*
- * CALLBACK :: end
- * ============================================= */
+  /*
+  * CALLBACK :: end
+  * ============================================= */
 
-	/**
-  * @name initNative
-  * @description Init all method
-  */
-	var initNative = function initNative() {
-		// default
-		initPreventBehavior();
-		// ==========================================
+  /**
+   * @name initNative
+   * @description Init all method
+   */
+  var initNative = function initNative() {
+    // default
+    initPreventBehavior();
+    // ==========================================
 
-		// lib
-		// ==========================================
+    // lib
+    initSwiper();
+    // ==========================================
 
-		// callback
-		// ==========================================
-	};
-	initNative();
+    // callback
+    // ==========================================
+  };
+  initNative();
 })();
